546423414edf50514d384eebd15903d956fcf1d3
davidofug <davidwampamba@gmail.com>
2019-05-12 01:11:51 +0300

    Fixed admin notice

--- acfhfl.php
+++ acfhfl.php
@@ -1,10 +1,63 @@
 <?php
 /*
- * Plugin Name: ACF Label SHOW-HIDE
- * Description: The plugin adds a hide setting to the ACF field settings
- * Author: David of UG
- * Author URL: https://david.ug
- * Tags: ACF, Field, Setting, Hide 
+Plugin Name: ACF Hide Field Label setting
+Plugin URI: https://github.com/davidofug/acf-hide-field-label
+Description: The plugin adds a hide setting to the ACF field settings
+Version: 1.0.1
+Author: David of UG
+Author URI: https://david.ug
+Text Domain: acf-hide-fied-label
+Domain Path: /lang
+License: GPL
 */
 
- require_once 'inc/acf-label-show-or-hide.php';
+if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+define('ACF_HIDE_FIELD_LABEL_SETTING_FILE', __FILE__);
+define('ACF_HIDE_FIELD_LABEL_SETTING_DIR', __DIR__);
+define('ACF_HIDE_LABEL_SETTING_PLUGIN_NAME','ACF hide field label');
+define('PLUGIND_VERSION','1.0.1');
+define('ACF_REQUIRED_VERSION','4');
+define('PHP_REQUIRED_VERSION','5');
+define('WP_REQUIRED_VERSION','4.5');
+
+if(is_admin()) :
+
+	function php_version_notice() {
+	?>
+		<div class="notice notice-error is-dismissible">
+			<p><?php _e( 'This plugin requires PHP 5.4+. Your PHP Version: '.phpversion(), 'acf-hide-field-label' ); ?></p>
+		</div>
+	<?php	
+    }
+    
+	function wp_version_notice() {
+	?>
+		<div class="notice notice-error is-dismissible">
+			<p><?php _e( 'This plugin requires WordPress version 4.5+ '.phpversion(), 'acf-hide-field-label' ); ?></p>
+		</div>
+	<?php	
+    }
+    
+	function acf_not_installed_notice() {
+	?>
+		<div class="notice notice-error is-dismissible">
+			<p><?php _e( ACF_HIDE_LABEL_SETTING_PLUGIN_NAME.' plugin requires the ACF Plugin.', 'acf-hide-field-label' ); ?></p>
+		</div>
+	<?php
+	}
+
+	if ( !version_compare( phpversion(), PHP_REQUIRED_VERSION, '>=' ) ) {
+		if( function_exists( 'php_version_notice') ) add_action( 'admin_notices', 'php_version_notice' );
+	}else{
+
+		if ( ! in_array( 'advanced-custom-fields-pro/acf.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+			if( function_exists( 'acf_not_installed_notice') ) add_action( 'admin_notices', 'acf_not_installed_notice' );
+
+		}else{
+			require_once ACF_HIDE_FIELD_LABE_LSETTING_DIR.'/inc/hide-label.php';
+		}
+
+	}
+
+endif;
--- inc/hide-label.php
+++ inc/hide-label.php
@@ -1,34 +1,33 @@
 
 <?php
-if(!function_exists('switch_label_render_field_settings')) :
+    if(!function_exists('acf_hide_label_render_field_settings')) :
 
-    function switch_label_render_field_settings( $field ) {	
-        acf_render_field_setting( $field, [
-            'label'			=> __('Hide Label?'),
-            'instructions'	=> 'If you don\'t want to show the label of the field.' ,
-            'name'			=> 'hide_label',
-            'type'			=> 'true_false',
-            'ui'			=> 1,
-        ], true);	
-    }
-    add_action('acf/render_field_settings', 'switch_label_render_field_settings');
-endif;
+        function switch_label_render_field_settings( $field ) {	
+            acf_render_field_setting( $field, [
+                'label'			=> __('Hide Label?','acf-hide-field-label'),
+                'instructions'	=> __('Allows you to hide field Label.','acf-hide-field-label'),
+                'name'			=> 'hide_label',
+                'type'			=> 'true_false',
+                'ui'			=> 1,
+            ], true);	
+        }
+        add_action('acf/render_field_settings', 'acf_hide_label_render_field_settings');
 
+    endif;
 
-if ( !function_exists('switch_label_prepare_field') ) :
-    
-    function switch_label_prepare_field( $field ) {
-        if ($field['hide_label'])
-            echo '
-            <style type="text/css">
-                .acf-field-', substr($field['key'],6),' > .acf-label {display: none;}
-            </style>';
-        return $field;
-    }
-    add_filter('acf/prepare_field', 'switch_label_prepare_field');
+    if ( !function_exists('acf_hide_label_prepare_field') ) :
+        
+        function acf_hide_label_prepare_field( $field ) {
+            if (isset($field['hide_label'])) :
+                echo '
+                <style type="text/css">
+                    .acf-field-', substr($field['key'],6),' > .acf-label {display: none;}
+                </style>';
+            endif;
 
-endif;
+            return $field;
+        }
 
+        add_filter('acf/prepare_field', 'acf_hide_label_prepare_field');
 
-
-
+    endif;

d79c6b287b53be6c427d855eef7e3045ff16857c
David Wampamba <davidwampamba@gmail.com>
2021-09-01 20:39:16 +0300

    Accessibility
    Accessibility implementation

--- inc/hide.label.php
+++ inc/hide.label.php
@@ -6,23 +6,25 @@ class Label {
 	
 	public function __construct() {
 		
-	    add_action('acf/render_field_settings', [$this,'renderFieldSettings']);
-    	add_filter('acf/prepare_field', [$this,'prepareField']);
+		add_action('acf/render_field_settings', [$this,'renderFieldSettings']);
+    		add_filter('acf/prepare_field', [$this,'prepareField']);
 		
 	}
 
 	public function renderFieldSettings( $field ) {
 		acf_render_field_setting( $field, [
-			'label'			=> __('Hide Label?','acf-hide-field-label'),
+			'aria-label'	=> __('Hide Label?','acf-hide-field-label'),
+			'label'		=> __('Hide Label?','acf-hide-field-label'),
 			'instructions'	=> __('Allows you to hide field Label.','acf-hide-field-label'),
-			'name'			=> 'hide_label',
-			'type'			=> 'true_false',
-			'ui'			=> 1,
+			'name'		=> 'hide_label',
+			'type'		=> 'true_false',
+			'ui'		=> 1,
 		], true);	
 	}
 
-	public function prepareField( $field ) {
+	/*public function prepareField( $field ) {
 		if (@$field['hide_label']) :
+		
 			echo '
 				<style type="text/css">
 					.acf-field-', substr($field['key'],6),' > .acf-label {display: none;}
@@ -30,6 +32,16 @@ class Label {
 				';
 		endif;
 
+		return $field;
+	}*/
+	
+	public function prepareField( $field ) {
+		if (@$field['hide_label']) : ?>
+			<style type="text/css">
+				.acf-field-<?= substr( $field['key'], 6); ?> > .acf-label {display: none;}
+			</style>
+		<?php endif;
+		
 		return $field;
 	}
 }

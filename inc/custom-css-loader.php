2d9bac0607dec57a86c860121e8dc5aec10ab359
Bunty <bhargav@buddyboss.com>
2022-03-29 21:56:17 +0530

    Fix phpcs

--- inc/custom-css-loader.php
+++ inc/custom-css-loader.php
@@ -1,9 +1,9 @@
 <?php
-if( !function_exists('acfls_acf_admin_head_custom_styles')) : 
-    function acfls_acf_admin_head_custom_styles() {
+if ( ! function_exists( 'acfls_acf_admin_head_custom_styles' ) ) :
+	function acfls_acf_admin_head_custom_styles() {
 
-    }
-    
-    add_action('acf/input/admin_head', 'acfls_acf_admin_head_custom_styles');
+	}
+
+	add_action( 'acf/input/admin_head', 'acfls_acf_admin_head_custom_styles' );
 
 endif;
